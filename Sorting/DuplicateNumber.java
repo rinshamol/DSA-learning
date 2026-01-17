ae84c77858bdd56f3df2d51fa5b32396df44dff4
rinshamol <rinshamolks65@gmail.com>
2026-01-17 23:13:20 +0530

    Leetcode medium level question

--- Sorting/DuplicateNumber.java
+++ Sorting/DuplicateNumber.java
@@ -1,25 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class DuplicateNumber {
     public  static void  main(String[] args) {
-        int[] arr = {1,3,2,2,4};
-        int num = cyclic(arr);
-        System.out.println(num);
+        int[] arr = {1};
+        List<Integer> ans = cyclic(arr);
+        System.out.println(ans);
     }
-    public static int cyclic(int[] arr) {
+    public static List<Integer> cyclic(int[] arr) {
         int i = 0 ;
             while (i < arr.length) {
-                if(arr[i] != i+1) {
                 int correct = arr[i] - 1;
                 if (arr[i] != arr[correct]) {
                     swap(arr, i, correct);
-                } else {
-                   return arr[i];
-                }
-            }else {
+                }else {
                     i++;
                 }
-        }
-        return -1;
+            }
+        List<Integer> ans = new ArrayList<>();
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[j] != j+1){
+                    ans.add(arr[j]);
+                }
+
+            }
+        return ans;
     }
 
     public  static void  swap(int[] arr, int fist, int last) {
