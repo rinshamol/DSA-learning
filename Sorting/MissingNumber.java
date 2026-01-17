d06504c5c67789ee02054fa80191b485ab3e093c
rinshamol <rinshamolks65@gmail.com>
2026-01-17 18:55:30 +0530

    cyclic sort implemented and related question answers

---
+++ Sorting/MissingNumber.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class MissingNumber {
+    public  static  void main(String[] args) {
+        int[] arr = {2,4,1,0};
+        int num = CyclicSort(arr);
+        System.out.println(num);
+
+    }
+    public static int CyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if(arr[i] < arr.length && arr[i] != arr[correct]  ){
+                swap(arr,i,correct);
+            }else  {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[j] != j) {
+                return j;
+            }
+        }
+        return arr.length;
+    }
+    static void  swap(int[] arr, int first, int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+
+
+}
