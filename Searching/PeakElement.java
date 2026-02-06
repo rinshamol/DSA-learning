dad21a6f00f7e73998ae975b5d37e34db4c84a37
rinshamol <rinshamolks65@gmail.com>
2026-02-06 16:11:32 +0530

    binary search problems

---
+++ Searching/PeakElement.java
@@ -0,0 +1,22 @@
+public class PeakElement {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,40};
+        int ans = search(arr);
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end ) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid+1]) {
+               end = mid;
+            } else  {
+                start = mid + 1;
+            }
+        }
+        return start;
+
+    }
+}
